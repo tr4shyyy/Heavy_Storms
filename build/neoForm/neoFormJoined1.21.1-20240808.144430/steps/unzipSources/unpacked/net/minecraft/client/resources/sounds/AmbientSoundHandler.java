a71cccff9b6d9e41a2500824017a8ef55a90b246
nolybutt <nolynn.rn@gmail.com>
2025-11-06 18:37:25 -0500

    cleaning up junk files

--- build/neoForm/neoFormJoined1.21.1-20240808.144430/steps/unzipSources/unpacked/net/minecraft/client/resources/sounds/AmbientSoundHandler.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.client.resources.sounds;
-
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
-
-@OnlyIn(Dist.CLIENT)
-public interface AmbientSoundHandler {
-    void tick();
-}
--- build/neoForm/neoFormJoined1.21.1-20240808.144430/steps/unzipSources/unpacked/net/minecraft/world/level/storage/WritableLevelData.java
+++
@@ -1,7 +0,0 @@
-package net.minecraft.world.level.storage;
-
-import net.minecraft.core.BlockPos;
-
-public interface WritableLevelData extends LevelData {
-    void setSpawn(BlockPos p_78649_, float p_78650_);
-}
